7402b87a9314445e8d1fdd20bb6951073a594299
Brian Munger <btmunger@gmail.com>
2025-05-21 00:27:31 -0700

    Combined like functions to be modular, moved around logic

--- checkstock.py
+++ checkstock.py
@@ -13,7 +13,7 @@ options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--ignore-certificate-errors")
 
-service = Service(r"C:\Users\btmun\OneDrive\Desktop\StockChecker\chromedriver-win64\chromedriver.exe")
+service = Service(r"C:\Users\btmun\OneDrive\Desktop\StockData\chromedriver-win64\chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=options)
 
 NVDU_SHARES = 2.01
@@ -25,42 +25,22 @@ days_ran = datetime.strptime(today.strftime("%Y-%m-%d"), "%Y-%m-%d")
 delta = days_ran - start_date
 days_passed = delta.days
 
-def get_NVDU_range(): 
-    driver.get("https://beta.finance.yahoo.com/quote/NVDU/")
+# Method to get the high/low price of the NVDU stock today
+def get_stock_range(stock_code): 
+    url = "https://beta.finance.yahoo.com/quote/" + stock_code + "/"
+    driver.get(url)
 
-    nvdu_range_element = WebDriverWait(driver, 5).until(
+    stock_range_element = WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "fin-streamer[data-field='regularMarketDayRange']"))
     )
 
-    nvdu_range = nvdu_range_element.text
-    parts = nvdu_range.split(" - ")
-    global nvdu_low
-    global nvdu_high
-    nvdu_low = float(parts[0])
-    nvdu_high = float(parts[1])
+    stock_range = stock_range_element.text
+    parts = stock_range.split(" - ")
 
-def get_WBD_range():
-    driver.get("https://beta.finance.yahoo.com/quote/WBD/")
+    return parts
 
-    wbd_range_element = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "fin-streamer[data-field='regularMarketDayRange']"))
-    )
-
-    wbd_range = wbd_range_element.text
-    parts = wbd_range.split(" - ")
-
-    global wbd_low
-    global wbd_high
-    wbd_low = float(parts[0])
-    wbd_high = float(parts[1])
-
-
-def write_csv():
-    global nvdu_high
-    global nvdu_low
-    global wbd_high
-    global wbd_low
-
+# Method for writing report to the CSV 
+def write_csv(nvdu_low, nvdu_high, wbd_low, wbd_high):
     data = [
         [days_passed, nvdu_high, nvdu_low, wbd_high, wbd_low]
     ]
@@ -69,8 +49,14 @@ def write_csv():
         writer = csv.writer(file)
         writer.writerows(data)
 
-get_NVDU_range()
-get_WBD_range()
-write_csv()
+nvdu_range = get_stock_range("NVDU")
+nvdu_low = float(nvdu_range[0])
+nvdu_high = float(nvdu_range[1])
+
+wbd_range = get_stock_range("WBD")
+wbd_low = float(wbd_range[0])
+wbd_high = float(wbd_range[1])
+
+write_csv(nvdu_low, nvdu_high, wbd_low, wbd_high)
 
 driver.quit()
