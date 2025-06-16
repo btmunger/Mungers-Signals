c082f079ccf27e19d7497d4ddf0647c3fc00e0e9
Brian Munger <btmunger@gmail.com>
2025-06-16 01:59:55 -0700

    Structured AI Analysis file - loads json file and converts most recent entry into a data object

--- ai_analysis.py
+++ ai_analysis.py
@@ -1,4 +1,21 @@
 # A project by Brian Munger
 
-def get_AI_advice():
-    return
+import json
+import pandas as pd
+from datetime import datetime
+
+# Method to return to retrieve a json file and return the most up to date entry
+def load_json(stock_code):
+    json_name = fr"trend_reports/{stock_code}_reports.json"
+
+    with open(json_name, "r") as file:
+        data = json.load(json_name)
+    
+    latest_entry = max(data, key=lambda x: datetime.fromisoformat(x["timestamp"]))
+
+    return latest_entry
+
+
+# Main method for the AI analysis portion of the project
+def ai_analysis(stock_code):
+    entry = load_json
