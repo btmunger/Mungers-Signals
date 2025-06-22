82b8e19da6d17ebd8ea9e56548fffab15c47ef59
btmunger <btmunger@gmail.com>
2025-06-22 14:57:33 -0700

    local commit

--- stock_tracker.py
+++ stock_tracker.py
@@ -6,7 +6,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 from datetime import datetime
 import csv
 import sys
