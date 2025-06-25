fea678ae48a8d46aad3147486706a0b99bf7cac3
btmunger <btmunger@gmail.com>
2025-06-25 00:48:22 -0700

    Most ofAI training algo completed

--- ai_train.py
+++ ai_train.py
@@ -3,13 +3,20 @@ import pandas as pd
 from datetime import datetime
 import os
 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+import joblib
+
 from transformers import pipeline
+
+entries_num = 0
 
 # Method for loading all trend report entries  
 def load_all_json():
     entries = []
     entries_added = 0
-
+ 
     for file_name in os.listdir('trend_reports'):
         full_path = os.path.join('trend_reports', file_name)
         if os.path.isfile(full_path):
@@ -107,12 +114,57 @@ def auto_label_entry(entry):
 def label_entries(processed_entries):
     for entry in processed_entries:
         entry["label"] = auto_label_entry(entry)
+        entries_num += 1
 
     return processed_entries
 
 # Method for training the model
 def train_model(data_frame):
-    return
+    # Select features 
+    feature_columns = [
+        "avg_percent_changes.close_past_week",
+        "avg_percent_changes.close_past_month",
+        "avg_percent_changes.open_past_week",
+        "avg_percent_changes.open_past_month",
+        "avg_percent_changes.volume_past_week",
+        "avg_percent_changes.volume_past_month",
+        "ranges.high_low_range_week",
+        "ranges.high_low_range_month",
+        "moving_avgs.sma_difference",
+        "moving_avgs.sma_ratio",
+        "standard_dev_calcs.closing_std_week",
+        "standard_dev_calcs.closing_std_month",
+        "standard_dev_calcs.zscore_week",
+        "standard_dev_calcs.zscore_month",
+        "headline_sentiment.headline_1",
+        "headline_sentiment.headline_2"
+    ]
+
+    # Input features
+    x_features = data_frame[feature_columns]
+    y_features = data_frame["label"].map({"buy": 1, "hold": 0, "sell": -1})
+
+    # **Train/test split**
+    # test_size: controls how much of the data is used for testing (instead of training)
+    # .20 would indicate 20% of data for testing, 80% of data for training
+    # random_state: seed number for random shuffling of data before splitting
+    # set this to get the same split every time
+    x_train, x_test, y_train, y_test = train_test_split(
+        x_features, y_features, test_size=0.2, random_state=42
+        )
+    
+    # Random forest classifier (to improve predicition accuracy)
+    # Works by training multiple decision trees on random subsets of features listed above
+    model = RandomForestClassifier(n_estimators=100, random_state=32)
+    model.fit(x_train, y_train)
+
+    # Generate predictions from a trained machine model
+    y_pred = model.predict(x_test)
+    print(classification_report(y_test, y_pred))
+
+    # Save model, print success message 
+    joblib.dump(model, "trained_stock_model.pkl")
+    print("Model trained on ")
 
 # Main method for calling helper functions to train the model
 def train_main():
@@ -121,5 +173,8 @@ def train_main():
         processed_entries = convert_headlines(data)
         labeled_entries = label_entries(processed_entries)
         data_frame = pd.json_normalize(labeled_entries)
+
+        data_frame["headline_sentiment.headline_1"] = data_frame["headline_sentiment.headline_1"].map({"positive": 1, "neutral": 0, "negative": -1})
+        data_frame["headline_sentiment.headline_2"] = data_frame["headline_sentiment.headline_2"].map({"positive": 1, "neutral": 0, "negative": -1})
 
         train_model(data_frame)
