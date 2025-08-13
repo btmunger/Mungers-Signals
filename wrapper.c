9c0564b721ad139475fecf6f79b8b516e6cbf633
Brian Munger <btmunger@gmail.com>
2025-08-12 17:26:28 -0700

    Adding directory and wrapper file for reference

---
+++ wrapper.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <Python.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    // Initialize the Python interpreter
+    Py_Initialize();
+
+    // Relative path to main script
+    const char *script_path = "./bootstrap.py";
+
+    // Open the Python script file
+    FILE *fp = fopen(script_path, "r");
+    if (fp != NULL) {
+        PyRun_SimpleFile(fp, script_path);
+        fclose(fp);
+    } else {
+        printf("Could not open Python script: %s\n", script_path);
+    }
+
+    // Finalize the Python interpreter
+    Py_Finalize();
+
+    return 0;
+}
+
+// gcc wrapper.c gui/icon/icon.res -IC:/Python313/include -LC:/Python313/libs -lpython313 -o MungersSignals.exe -mwindows
